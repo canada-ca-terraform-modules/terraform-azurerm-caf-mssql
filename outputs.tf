8ec993908855476ac427d85520de0f58d3e20fb2
bernardmaltais <bernard.maltais@ssc-spc.gc.ca>
2021-11-26 08:35:33 -0500

    Update (#1)
    Adding support for new mssql features
    Now requires the use of a group variable

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,10 @@
-# output "id" {
-#   value = azurerm_mssql_server.mssql.id
-# }
-
-# output "name" {
-#   value = azurerm_mssql_server.mssql.name
-# }
-
-# output "administrator_login" {
-#   value = azurerm_mssql_server.mssql.administrator_login
-# }
-
-output "server" {
-  value = var.server
+output "mssql_server" {
+  value = azurerm_mssql_server.mssql
 }
 
+# output "server" {
+#   value = var.server
+# }
 
 # Part of a hack for module-to-module dependencies.
 # https://github.com/hashicorp/terraform/issues/1178#issuecomment-449158607
